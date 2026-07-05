887ae422029937f796b933ca90f573133b5b6486
Hafiz Ayoob <51916297+ManiBasha@users.noreply.github.com>
2026-07-05 08:43:29 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -20,12 +20,3 @@ export const firebaseConfig = {
 
 // Used to detect an unconfigured project and show a clear setup banner
 // instead of failing silently.
-export const isFirebaseConfigured = () =>
-  firebaseConfig.apiKey && firebaseConfig.apiKey !== "YOUR_API_KEY" &&
-  firebaseConfig.projectId && firebaseConfig.projectId !== "YOUR_PROJECT_ID";
-  apiKey:            "AIzaSyCfS59K-LYm_zXrWJEYVQ9IRID4al0wkws",
-  authDomain:        "capitalone-finance.firebaseapp.com",
-  projectId:         "capitalone-finance",
-  storageBucket:     "capitalone-finance.firebasestorage.app",
-  messagingSenderId: "344388986779",
-  appId:             "1:344388986779:web:5e7d410c49842e2e5fe33e"
