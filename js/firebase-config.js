80b4000d924133dc11f5b0aff7157bff425486c5
Hafiz Ayoob <51916297+ManiBasha@users.noreply.github.com>
2026-07-05 14:44:26 +0300

    Add Firebase configuration validation function

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,3 +10,7 @@ export const firebaseConfig = {
   messagingSenderId: "344388986779",
   appId:             "1:344388986779:web:5e7d410c49842e2e5fe33e"
 };
+
+export const isFirebaseConfigured = () =>
+  firebaseConfig.apiKey && firebaseConfig.apiKey !== "YOUR_API_KEY" &&
+  firebaseConfig.projectId && firebaseConfig.projectId !== "YOUR_PROJECT_ID";
